0d279a28e51e16c8d2666ec489c8170b0cfd93bc
Marcelo Ballester <marceloballester@gmail.com>
2025-01-14 19:21:14 +0100

    Cambio de Marcelo

--- MiClase1.java
+++ MiClase1.java
@@ -6,6 +6,7 @@ public class MiClase1
         {
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
             System.out.println("¿Como estas?");
+            System.out.println("Cambio de Marcelo");
         }
     }
 }
